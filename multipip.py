96d4b9d8c289037d81d59b00d3a1a9806775c6dc
Pahima Smash <502michlol@gmail.com>
2023-02-15 08:09:27 +0200

    initial commit. No code yet

---
+++ multipip.py
@@ -0,0 +1,14 @@
+#########################################
+# "multipip.py"                         #
+#                                       #
+# Download modules for multiple Python  #
+# versions and OS distributions from    #
+# the official PyPI website.            #
+#########################################
+
+# Imports
+import requests
+import shutil
+
+# Constants
+BASE_URL = 
